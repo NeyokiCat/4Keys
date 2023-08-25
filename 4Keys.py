269ef47b58f761406a276c71eaaa8233badca019
Neyoki <neyoki0the0cat@foxmail.com>
2023-08-25 14:14:34 +0800

    20230825
    Add Search Function

--- 4Keys.py
+++ 4Keys.py
@@ -1,14 +1,13 @@
 import math
-import csv
+import pandas as pd
+import os
 import secrets
 import string
 
-def main():
-    # Start off interface
-        # Generate
-        # Search 
-        # Statistics
-    print()
+#inits
+PATH = os.path.dirname(__file__)
+PATH_to_keys = PATH+'/Keys'
+startOff = '''Welcome to 4Keys\nGenerate a new key(1), Search for a key (2), Show key statistics (3)'''
 
 # Patterns
 # numbers = string.digits
@@ -37,9 +36,65 @@ TokenM = secrets.token_urlsafe()
 def generateRul(specRul):
     return specRul
     
+# Search
+def search():
+    for (root, dirs, k) in os.walk(PATH_to_keys):
+        # print(root)
+        root += '/'
+        # print(dirs)
+        for f in k:
+            f = root + f
+            #print(f)
+            if '.csv' in f:
+                return [f,'csvFile']
+            else:
+                try:
+                    open(f, 'r',encoding = 'utf-8' )
+                    return [f,'txtFile']
+                except:
+                    None
+        
+def choice(c):
+    # Generate
+    if c == '1':
+        pass
+    
+    # Search 
+    if c == '2':
+        res = search()
+        print('find ' + res[1] + ' at \'' + res[0] + '\'')
+        c += input('continue (1), choose another path (2)')
+    
+    if c == '21':
+            if res[1] == 'csvFile':
+                with open(res[0], newline='') as csvfile:
+                    df = pd.read_csv(csvfile)
+                    keyword = input('Search for keyword:')
+                for row in df:
+                    print(df.loc[df[row] == keyword])
+
+    if c == '22':
+        pass
+    
+    # Statistics
+    if c == '3':
+          pass
+    
+  
+    
+def main(ans = ''):
+    # Start-off interface
+    if ans != '':
+        choice(ans)
+    else:
+        main(input(startOff))
+    
 
 # Tests 
 # print(generate(12,NaAsM))
 # print(generate(6,Nonly))
 # print(generateRul(MacM))
 # print(generateRul(TokenM))
+# print(search()[1])
+
+main()
